a967147331700a6c005b3c56666bf79490626066
pyrozn <lucasprado4561@gmail.com>
2026-09-15 22:42:46 -0300

    Aula9 - PHP + HTML
    NAO FUNCIONA NADA.  😭

--- 4 - Aulas PHP/Aula 9 - PHP + HTML/exec1.php
+++ 4 - Aulas PHP/Aula 9 - PHP + HTML/exec1.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aula 2</title>
+    <title>Aula 2 - Exercício 1</title>
 </head>
 <body>
     <h1>Somar dois valor</h1>
